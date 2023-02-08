3e8a8c82a8aa7cec92d5bb9183823ede3843e68f
Mark Butler <mbutler@bfsmmbutler.navimedix.com>
2023-02-08 16:57:37 +0000

    the test input and output files are contained within the project

--- test/test.py
+++ test/test.py
@@ -5,10 +5,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from src.converter import converter
 
+
 from deleteTest import deleteTest
 
-Input = os.path.expanduser("~/testFile")
-Output = os.path.expanduser("~/outputTestFile")
+# Input = os.path.expanduser("~/testFile")
+Input = os.path.dirname(__file__) + "/testFile"
+Output = os.path.dirname(__file__) + "/outputTestFile"
 
 
 class TestStringMethods(unittest.TestCase):
@@ -20,14 +22,9 @@ class TestStringMethods(unittest.TestCase):
         browser = webdriver.Firefox()
         browser.get(r'file:///Users/mbutler/outputTestFile/test.html')
         # try to find the word apple in the output of the json file
-        apple = browser.find_element(By.CSS_SELECTOR, "body > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)")
+        apple = browser.find_element(By.CSS_SELECTOR,"body > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)")
         # asserts if the word apple is found
         self.assertEqual(apple.text, "Apple")
         browser.quit()
         # delete output contents
-        path_to_folder = "//Users/mbutler/outputTestFile"
-        deleteTest(path_to_folder)
-
-
-
-
+        deleteTest(Output)
