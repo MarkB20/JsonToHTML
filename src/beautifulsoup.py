ff606b9e4b6e7eb48bfddb98719044e5850ba66e
mbutler <w.markbutler2001@gmail.com>
2023-02-13 13:36:09 +0000

    stylesheet working

--- src/beautifulsoup.py
+++ src/beautifulsoup.py
@@ -9,10 +9,14 @@ def bs(Output):
     file = open(Output)
     cssPath=os.path.dirname(__file__)+"/stylesheet.css"
     soup=bs4.BeautifulSoup(file.read(),"html.parser")
-    for tag in soup.table.find_all(['table']):
-        if tag.text == 'fruit':
-            tag.wrap(soup.new_tag('a'))
+    #Path = f'"{cssPath}"'
+    Path = f'"stylesheet.css"'
+    textCss = '"text/css"'
+    stylesheet = '"stylesheet"'
 
+    #print(f"<link href= {Path} rel = {stylesheet} type= {textCss} ")
+
+    soup.table.wrap(soup.new_tag(f"link href= {Path} rel = {stylesheet} type= {textCss} "))
 
 
     #soup.table.string.wrap(soup.new_tag("div"))
